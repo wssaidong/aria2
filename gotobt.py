b44f4a5499592846bce76adfff9c33e6194da5ea
2449149803@qq.com <>
2022-05-08 19:01:09 +0800

    feat: kafka consumer

--- gotobt.py
+++ gotobt.py
@@ -1,1 +1,9 @@
 
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('log-movie',group_id='log-movie-consumer',bootstrap_servers=['192.168.3.90:9092'])
+for message in consumer:
+
+    print  (message.topic, 
+            message.partition,
+            message.offset, message.key,
+            message.value.decode('utf-8'))
